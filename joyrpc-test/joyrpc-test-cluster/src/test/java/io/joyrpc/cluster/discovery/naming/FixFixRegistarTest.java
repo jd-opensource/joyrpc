749e414bde49124f5a272be8a8961cb1cc961b8c
hexiaofeng <hexiaofeng@139.com>
2019-09-19 11:53:08 +0800

    修改测试代码

--- joyrpc-test/joyrpc-test-cluster/src/test/java/io/joyrpc/cluster/discovery/naming/FixFixRegistarTest.java
+++ joyrpc-test/joyrpc-test-cluster/src/test/java/io/joyrpc/cluster/discovery/naming/FixFixRegistarTest.java
@@ -9,9 +9,9 @@ package io.joyrpc.cluster.discovery.naming;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,17 +33,18 @@ import java.util.concurrent.CountDownLatch;
 
 public class FixFixRegistarTest {
 
-    protected static Registar registar = new FixRegistar(URL.valueOf("jmq://test?url=192.168.1.1,192.168.1.2,192.168.1.3;192.168.1.4,192.168.1.5,192.168.1.6?dataCenter=test&region=test"));
-
     @Test
     public void test() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(2);
         MyClusterHandler handler1 = new MyClusterHandler(latch);
         MyClusterHandler handler2 = new MyClusterHandler(latch);
-        URL url = URL.valueOf("jmq://topic1");
-        Assert.assertTrue(registar.subscribe(url, handler1));
-        Assert.assertFalse(registar.subscribe(url, handler1));
-        Assert.assertTrue(registar.subscribe(url, handler2));
+        URL url = URL.valueOf("joy://topic1");
+        FixRegistar registar = new FixRegistar(URL.valueOf("joy://test?address=192.168.1.1,192.168.1.6?dataCenter=test&region=test"));
+        registar.open().whenComplete((v, t) -> {
+            Assert.assertTrue(registar.subscribe(url, handler1));
+            Assert.assertFalse(registar.subscribe(url, handler1));
+            Assert.assertTrue(registar.subscribe(url, handler2));
+        });
         latch.await();
         Assert.assertEquals(handler1.count, 1);
         Assert.assertEquals(handler1.shards.size(), 2);

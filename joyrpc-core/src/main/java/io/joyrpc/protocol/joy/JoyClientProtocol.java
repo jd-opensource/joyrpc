f4bf7a4724b5d6553ad9d89b9d4a0a6c90a1d18c
hexiaofeng <hexiaofeng@139.com>
2019-10-22 08:24:27 +0800

    review

--- joyrpc-core/src/main/java/io/joyrpc/protocol/joy/JoyClientProtocol.java
+++ joyrpc-core/src/main/java/io/joyrpc/protocol/joy/JoyClientProtocol.java
@@ -9,9 +9,9 @@ package io.joyrpc.protocol.joy;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -65,7 +65,7 @@ public class JoyClientProtocol extends AbstractProtocol implements ClientProtoco
     public Message negotiation(final URL clusterUrl, final Client client) {
         //构造协商请求
         NegotiationRequest negotiation = new NegotiationRequest(clusterUrl,
-                new NegotiationOption(Constants.SERIALIZATION_OPTION, "msgpack", "json0"),
+                new NegotiationOption(Constants.SERIALIZATION_OPTION, "msgpack", "json0", "json1"),
                 new NegotiationOption(Constants.COMPRESS_OPTION), null);
         //设置client本地session属性
         negotiation.addAttribute(Constants.CONFIG_KEY_INTERFACE, clusterUrl.getPath());

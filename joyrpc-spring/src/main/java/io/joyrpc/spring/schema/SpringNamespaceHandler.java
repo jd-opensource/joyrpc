9ced1582ffe1b28983a910ad03b245931e5b1ccd
hexiaofeng <hexiaofeng@139.com>
2019-11-06 18:45:16 +0800

    删除注解解析器

--- joyrpc-spring/src/main/java/io/joyrpc/spring/schema/SpringNamespaceHandler.java
+++ joyrpc-spring/src/main/java/io/joyrpc/spring/schema/SpringNamespaceHandler.java
@@ -35,6 +35,5 @@ public class SpringNamespaceHandler extends NamespaceHandlerSupport {
         registerBeanDefinitionParser("server", new ServerBeanDefinitionParser());
         registerBeanDefinitionParser("registry", new RegistryBeanDefinitionParser());
         registerBeanDefinitionParser("parameter", new GlobalParameterDefinitionParser());
-        //registerBeanDefinitionParser("annotation", new AnnotationBeanDefinitionParser());
     }
 }

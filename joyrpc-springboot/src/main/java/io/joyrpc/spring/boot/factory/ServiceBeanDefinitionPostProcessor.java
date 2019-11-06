2fcad90df0d6d027131027afe7eb0a12747c0339
hexiaofeng <hexiaofeng@139.com>
2019-11-06 19:16:32 +0800

    review

--- joyrpc-springboot/src/main/java/io/joyrpc/spring/boot/factory/ServiceBeanDefinitionPostProcessor.java
+++ joyrpc-springboot/src/main/java/io/joyrpc/spring/boot/factory/ServiceBeanDefinitionPostProcessor.java
@@ -20,6 +20,7 @@ package io.joyrpc.spring.boot.factory;
  * #L%
  */
 
+import io.joyrpc.config.AbstractIdConfig;
 import io.joyrpc.context.GlobalContext;
 import io.joyrpc.extension.ExtensionPoint;
 import io.joyrpc.extension.ExtensionPointLazy;
@@ -28,10 +29,8 @@ import io.joyrpc.spring.annotation.Provider;
 import io.joyrpc.spring.boot.context.DefaultClassPathBeanDefinitionScanner;
 import io.joyrpc.spring.boot.processor.AnnotationBeanDefinitionProcessor;
 import io.joyrpc.spring.boot.processor.MergePropertiesProcessor;
-import io.joyrpc.spring.boot.processor.RpcPropertiesProcessor;
 import io.joyrpc.spring.boot.properties.MergeServiceBeanProperties;
 import io.joyrpc.spring.boot.properties.RpcProperties;
-import io.joyrpc.spring.util.PropertySourcesUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -41,9 +40,12 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.type.ClassMetadata;
 import org.springframework.core.type.classreading.MetadataReader;
@@ -57,11 +59,12 @@ import org.springframework.validation.DataBinder;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static io.joyrpc.constants.Constants.PROTOCOL_KEY;
+import static io.joyrpc.spring.boot.processor.AnnotationBeanDefinitionProcessor.REGISTRY_NAME;
+import static io.joyrpc.spring.boot.processor.AnnotationBeanDefinitionProcessor.SERVER_NAME;
 import static org.springframework.util.ClassUtils.resolveClassName;
 
 /**
@@ -73,13 +76,11 @@ public class ServiceBeanDefinitionPostProcessor implements BeanDefinitionRegistr
 
     public static final String BEAN_NAME = "serviceBeanDefinitionPostProcessor";
 
-    public static final String PREFIX = GlobalContext.getString(PROTOCOL_KEY);
-
     private static final ExtensionPoint<AnnotationBeanDefinitionProcessor, String> REGISTRY_PROCESSOR = new ExtensionPointLazy<>(AnnotationBeanDefinitionProcessor.class);
 
     protected RpcProperties rpcProperties;
 
-    protected Environment environment;
+    protected ConfigurableEnvironment environment;
 
     protected ResourceLoader resourceLoader;
 
@@ -89,34 +90,36 @@ public class ServiceBeanDefinitionPostProcessor implements BeanDefinitionRegistr
 
     protected MergeServiceBeanProperties mergeProperties;
 
-    protected RpcPropertiesProcessor rpcPropertiesProcessor;
-
     protected MergePropertiesProcessor mergePropertiesProcessor;
 
 
     /**
      * 构造方法
      */
-    public ServiceBeanDefinitionPostProcessor(ApplicationContext applicationContext, Environment environment, ResourceLoader resourceLoader) {
+    public ServiceBeanDefinitionPostProcessor(final ApplicationContext applicationContext,
+                                              final ConfigurableEnvironment environment,
+                                              final ResourceLoader resourceLoader) {
         this.applicationContext = applicationContext;
         this.environment = environment;
         this.resourceLoader = resourceLoader;
         this.rpcProperties = new RpcProperties();
         //读取rpc为前缀的配置
-        Map<String, Object> objectMap = PropertySourcesUtils.getSubProperties((ConfigurableEnvironment) environment, PREFIX);
+        Map<String, Object> objectMap = getProperties();
         //绑定数据
         DataBinder dataBinder = new DataBinder(rpcProperties);
         MutablePropertyValues propertyValues = new MutablePropertyValues(objectMap);
         dataBinder.bind(propertyValues);
         this.mergeProperties = new MergeServiceBeanProperties(environment, rpcProperties);
-        this.rpcPropertiesProcessor = new RpcPropertiesProcessor();
         this.mergePropertiesProcessor = new MergePropertiesProcessor();
     }
 
     @Override
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-        //处理配置信息
-        rpcPropertiesProcessor.processProperties(registry, rpcProperties);
+        //注册
+        register(registry, rpcProperties.getRegistry(), REGISTRY_NAME);
+        register(registry, rpcProperties.getRegistries(), REGISTRY_NAME);
+        register(registry, rpcProperties.getServer(), SERVER_NAME);
+        register(registry, rpcProperties.getServers(), SERVER_NAME);
         //收集packagesToScan配置，获取rpc要扫描的包路径
         Set<String> packages = new LinkedHashSet<>();
         if (rpcProperties.getPackages() != null) {
@@ -169,6 +172,83 @@ public class ServiceBeanDefinitionPostProcessor implements BeanDefinitionRegistr
     @Override
     public void setBeanClassLoader(ClassLoader classLoader) {
         this.classLoader = classLoader;
+    }
+
+    /**
+     * 注册
+     *
+     * @param registry      注册表
+     * @param configs       多个配置
+     * @param defNamePrefix 默认名称
+     */
+    protected <T extends AbstractIdConfig> void register(final BeanDefinitionRegistry registry,
+                                                         final List<T> configs, final String defNamePrefix) {
+        if (configs != null) {
+            AtomicInteger counter = new AtomicInteger(0);
+            for (T config : configs) {
+                register(registry, config, defNamePrefix + "-" + counter.getAndIncrement());
+            }
+        }
+    }
+
+    /**
+     * 注册
+     *
+     * @param registry
+     * @param config
+     * @param defName
+     * @param <T>
+     */
+    protected <T extends AbstractIdConfig> void register(final BeanDefinitionRegistry registry, final T config,
+                                                         final String defName) {
+        if (config == null) {
+            return;
+        }
+        String beanName = config.getId();
+        if (!StringUtils.hasText(beanName)) {
+            beanName = defName;
+        }
+        if (!registry.containsBeanDefinition(beanName)) {
+            //TODO 要验证是否正确注入了环境变量
+            registry.registerBeanDefinition(beanName, new RootBeanDefinition((Class<T>) config.getClass(), () -> config));
+        }
+    }
+
+    /**
+     * Get Sub {@link Properties}
+     *
+     * @return Map
+     * @see Properties
+     */
+    public Map<String, Object> getProperties() {
+
+        Map<String, Object> subProperties = new LinkedHashMap<String, Object>();
+
+        MutablePropertySources propertySources = environment.getPropertySources();
+
+        String prefix = GlobalContext.getString(PROTOCOL_KEY);
+        prefix = prefix.endsWith(".") ? prefix : prefix + ".";
+
+        for (PropertySource<?> source : propertySources) {
+            if (source instanceof EnumerablePropertySource) {
+                for (String name : ((EnumerablePropertySource<?>) source).getPropertyNames()) {
+                    if (!subProperties.containsKey(name) && name.startsWith(prefix)) {
+                        String subName = name.substring(prefix.length());
+                        if (!subProperties.containsKey(subName)) { // take first one
+                            Object value = source.getProperty(name);
+                            if (value instanceof String) {
+                                // Resolve placeholder
+                                value = environment.resolvePlaceholders((String) value);
+                            }
+                            subProperties.put(subName, value);
+                        }
+                    }
+                }
+            }
+        }
+
+        return Collections.unmodifiableMap(subProperties);
+
     }
 
     /**

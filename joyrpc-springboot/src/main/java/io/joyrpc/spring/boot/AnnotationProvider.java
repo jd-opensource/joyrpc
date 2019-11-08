df0b0ed0332866e00a7a550c99b7a9d27fc27fbf
hexiaofeng <hexiaofeng@139.com>
2019-11-08 09:30:03 +0800

    review

--- joyrpc-springboot/src/main/java/io/joyrpc/spring/boot/AnnotationProvider.java
+++ joyrpc-springboot/src/main/java/io/joyrpc/spring/boot/AnnotationProvider.java
@@ -1,7 +1,7 @@
 package io.joyrpc.spring.boot;
 
-import io.joyrpc.annotation.Consumer;
-import io.joyrpc.annotation.Provider;
+import io.joyrpc.config.ConsumerConfig;
+import io.joyrpc.config.ProviderConfig;
 import io.joyrpc.extension.Extensible;
 
 import java.lang.annotation.Annotation;
@@ -13,124 +13,33 @@ import java.lang.annotation.Annotation;
 public interface AnnotationProvider {
 
     /**
-     * 服务提供者的注解
+     * 服务提供者的注解类
      *
      * @return
      */
-    Annotation getProvider();
+    Class<Annotation> getProviderAnnotationClass();
 
     /**
-     * 消费者的注解
+     * 消费者的注解类
      *
      * @return
      */
-    Annotation getConsumer();
+    Class<Annotation> getConsumerAnnotationClass();
 
     /**
-     * 服务提供者注解转换
-     *
-     * @param annotation
-     * @return
-     */
-    Provider toProvider(Annotation annotation);
-
-    /**
-     * 消费者注解转换
+     * 服务提供者注解转换成配置
      *
      * @param annotation
      * @return
      */
-    Consumer toConsumer(Annotation annotation);
+    ProviderConfig toProviderConfig(Annotation annotation);
 
     /**
-     * 服务提供者实现
+     * 消费者注解转换成配置
+     *
+     * @param annotation
+     * @return
      */
-    class ProviderAnnotation implements Annotation, Provider {
-        /**
-         * 名称
-         */
-        private String name;
-        /**
-         * 分组
-         */
-        private String alias;
-        /**
-         * 接口
-         */
-        private Class interfaceClass;
-
-        /**
-         * 构造函数
-         *
-         * @param name
-         * @param alias
-         * @param interfaceClass
-         */
-        public ProviderAnnotation(String name, String alias, Class interfaceClass) {
-            this.name = name;
-            this.alias = alias;
-            this.interfaceClass = interfaceClass;
-        }
-
-        @Override
-        public String name() {
-            return name;
-        }
-
-        @Override
-        public String alias() {
-            return alias;
-        }
-
-        @Override
-        public Class interfaceClass() {
-            return interfaceClass;
-        }
-
-        @Override
-        public Class<? extends Annotation> annotationType() {
-            return Provider.class;
-        }
-    }
-
-    /**
-     * 消费者实现
-     */
-    class ConsumerAnnotation implements Annotation, Consumer {
-        /**
-         * 名称
-         */
-        private String name;
-        /**
-         * 分组
-         */
-        private String alias;
-
-        /**
-         * 构造函数
-         *
-         * @param name
-         * @param alias
-         */
-        public ConsumerAnnotation(String name, String alias) {
-            this.name = name;
-            this.alias = alias;
-        }
-
-        @Override
-        public String name() {
-            return name;
-        }
-
-        @Override
-        public String alias() {
-            return alias;
-        }
-
-        @Override
-        public Class<? extends Annotation> annotationType() {
-            return Consumer.class;
-        }
-    }
+    ConsumerConfig toConsumerConfig(Annotation annotation);
 
 }

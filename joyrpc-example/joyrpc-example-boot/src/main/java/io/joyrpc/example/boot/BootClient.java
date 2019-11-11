ce68ed2ff8d108b4c163e634930e7cac704a73ad
hexiaofeng <hexiaofeng@139.com>
2019-11-11 10:49:56 +0800

    review

--- joyrpc-example/joyrpc-example-boot/src/main/java/io/joyrpc/example/boot/BootClient.java
+++ joyrpc-example/joyrpc-example-boot/src/main/java/io/joyrpc/example/boot/BootClient.java
@@ -4,18 +4,27 @@ import io.joyrpc.example.service.DemoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class BootClient {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         System.setProperty("spring.profiles.active", "client");
         ConfigurableApplicationContext run = SpringApplication.run(BootClient.class, args);
         DemoService consumer = run.getBean(DemoService.class);
         while (true) {
-            System.out.println(consumer.sayHello("helloWold"));
-            Thread.sleep(1000L);
+            try {
+                System.out.println(consumer.sayHello("helloWold"));
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                break;
+            } catch (Exception e) {
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException ex) {
+                }
+                e.printStackTrace();
+            }
         }
     }
 }

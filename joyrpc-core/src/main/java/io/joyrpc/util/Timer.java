4fbe841b83c03342b07623083ca883bce9755150
hexiaofeng <hexiaofeng@139.com>
2019-11-18 13:15:27 +0800

    review

--- joyrpc-core/src/main/java/io/joyrpc/util/Timer.java
+++ joyrpc-core/src/main/java/io/joyrpc/util/Timer.java
@@ -66,8 +66,8 @@ public class Timer {
         this.bossPool.submit(() -> {
             while (!Shutdown.isShutdown()) {
                 try {
-                    //TODO 停止的时候要及时中断
-                    Slot slot = queue.poll(timeWheel.getDuration(), TimeUnit.MILLISECONDS);
+                    //停止的时候要及时中断，把timeWheel.getDuration()改成了1000ms
+                    Slot slot = queue.poll(1000L, TimeUnit.MILLISECONDS);
                     if (slot != null && !Shutdown.isShutdown()) {
                         //推进时间
                         timeWheel.advance(slot.getExpiration());

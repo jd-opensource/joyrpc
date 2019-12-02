3c1ac0519aa2409343eec8616f982f667fc9d5e1
hexiaofeng <hexiaofeng@139.com>
2019-12-02 08:36:09 +0800

    comment

--- joyrpc-core/src/main/java/io/joyrpc/transport/Endpoint.java
+++ joyrpc-core/src/main/java/io/joyrpc/transport/Endpoint.java
@@ -9,9 +9,9 @@ package io.joyrpc.transport;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -141,9 +141,26 @@ public interface Endpoint extends AutoCloseable {
     }
 
     /**
+     * 状态
+     *
      * @date: 2019/1/14
      */
     enum Status {
-        CLOSED, OPENING, OPENED, CLOSING
+        /**
+         * 关闭
+         */
+        CLOSED,
+        /**
+         * 打开中
+         */
+        OPENING,
+        /**
+         * 打开
+         */
+        OPENED,
+        /**
+         * 关闭中
+         */
+        CLOSING
     }
 }

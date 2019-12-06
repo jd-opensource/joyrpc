f1ea3753aee628bc006e67819e2b2dfbe1a7053e
hexiaofeng <hexiaofeng@139.com>
2019-12-06 18:57:53 +0800

    review RestServer

--- joyrpc-example/joyrpc-example-boot/src/main/java/io/joyrpc/example/boot/BootRestServer.java
+++ joyrpc-example/joyrpc-example-boot/src/main/java/io/joyrpc/example/boot/BootRestServer.java
@@ -1,17 +1,13 @@
 package io.joyrpc.example.boot;
 
-import io.joyrpc.config.Warmup;
 import io.joyrpc.example.service.DemoService;
 import io.joyrpc.example.service.impl.RestDemoServiceImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-
+@SpringBootApplication
 public class BootRestServer {
-
-    private static final Logger logger = LoggerFactory.getLogger(BootRestServer.class);
 
     @Bean("restDemoServiceImpl")
     public DemoService restDemoServiceImpl() {

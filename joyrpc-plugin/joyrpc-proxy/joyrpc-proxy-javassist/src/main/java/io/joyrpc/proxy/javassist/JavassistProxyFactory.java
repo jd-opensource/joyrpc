4c44583481c6955c2d20347942634dff91c27603
yangyang36 <yangyang36@jd.com>
2019-12-18 11:41:44 +0800

    修复javassist代理，数组参数的拼接错误的问题

--- joyrpc-plugin/joyrpc-proxy/joyrpc-proxy-javassist/src/main/java/io/joyrpc/proxy/javassist/JavassistProxyFactory.java
+++ joyrpc-plugin/joyrpc-proxy/joyrpc-proxy-javassist/src/main/java/io/joyrpc/proxy/javassist/JavassistProxyFactory.java
@@ -9,9 +9,9 @@ package io.joyrpc.proxy.javassist;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -252,10 +252,16 @@ public class JavassistProxyFactory implements ProxyFactory {
          * @return name.
          */
         protected StringBuilder getName(Class<?> c, final StringBuilder builder) {
-            builder.append(c.getName());
-            while (c.isArray()) {
-                builder.append("[]");
-                c = c.getComponentType();
+            if (c.isArray()) {
+                StringBuilder suffixSb = new StringBuilder();
+                Class<?> ct = c;
+                while (ct.isArray()) {
+                    ct = ct.getComponentType();
+                    suffixSb.append("[]");
+                }
+                builder.append(ct.getName()).append(suffixSb);
+            } else {
+                builder.append(c.getName());
             }
             return builder;
         }

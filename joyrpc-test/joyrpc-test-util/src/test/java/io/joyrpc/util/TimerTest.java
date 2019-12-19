d8230d5c6dba78f77debdc04472f997a56148539
hexiaofeng <hexiaofeng@139.com>
2019-12-19 20:00:52 +0800

    增加测试用例

--- joyrpc-test/joyrpc-test-util/src/test/java/io/joyrpc/util/TimerTest.java
+++ joyrpc-test/joyrpc-test-util/src/test/java/io/joyrpc/util/TimerTest.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class TimerTest {
     protected static final Logger logger = LoggerFactory.getLogger(TimerTest.class);
@@ -12,11 +13,14 @@ public class TimerTest {
     @Test
     public void testTimeWheel() throws InterruptedException {
 
-        Timer timer = new Timer("default", 100, 5, 3);
+        int tickTime = 100;
+        Timer timer = new Timer("default", tickTime, 5, 3);
         CountDownLatch latch = new CountDownLatch(6);
         timer.add("test", SystemClock.now(), new MyTask("1", latch));
         timer.add("test", SystemClock.now() + 200, new MyTask("2", latch));
         timer.add("test", SystemClock.now() + 200, new MyTask("3", latch));
+        //1跳
+        latch.await(tickTime, TimeUnit.MILLISECONDS);
         timer.add("test", SystemClock.now() + 600, new MyTask("4", latch));
         timer.add("test", SystemClock.now() + 1500, new MyTask("5", latch));
         timer.add("test", SystemClock.now() + 1500, new MyTask("6", latch));
@@ -35,7 +39,7 @@ public class TimerTest {
 
         @Override
         public void run() {
-            logger.info(name + " is running");
+            logger.info(name + " is running " + SystemClock.now());
             latch.countDown();
         }
     }

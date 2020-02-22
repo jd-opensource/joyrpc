6e43c045361affe11f455d38298d571b6074456c
hexiaofeng <hexiaofeng@139.com>
2020-02-22 18:22:27 +0800

    如果配置了网卡环境变量，初始化根据网卡设置本地地址，便于在VPN环境下的本地调试

--- joyrpc-core/src/main/java/io/joyrpc/util/network/Ipv4.java
+++ joyrpc-core/src/main/java/io/joyrpc/util/network/Ipv4.java
@@ -12,9 +12,9 @@ package io.joyrpc.util.network;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -95,6 +95,10 @@ public class Ipv4 {
          */
         try {
             LOCAL_IPS = new HashSet<>(getLocalIps());
+            //绑定到某个网卡上
+            if (NET_INTERFACE != null && !NET_INTERFACE.isEmpty()) {
+                localIp = getLocalIp(NET_INTERFACE, MANAGE_IP_DEF);
+            }
         } catch (SocketException e) {
         }
     }

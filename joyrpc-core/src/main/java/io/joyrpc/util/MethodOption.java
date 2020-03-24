6a3e8e5247e4d9972cc29cb8b703adac9940b9ed
hexiaofeng <hexiaofeng@139.com>
2020-03-24 08:51:23 +0800

    加注释，增加遍历方法，方便后续自适应负载均衡进行计算

--- joyrpc-core/src/main/java/io/joyrpc/util/MethodOption.java
+++ joyrpc-core/src/main/java/io/joyrpc/util/MethodOption.java
@@ -26,6 +26,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 /**
@@ -42,7 +43,7 @@ public class MethodOption<M, T> {
     /**
      * 名字作为Key的转换函数
      */
-    protected static final Function<Method, String> NAME_KEY = method -> method.getName();
+    protected static final Function<Method, String> NAME_KEY = Method::getName;
     /**
      * 方法缓存数据
      */
@@ -70,7 +71,7 @@ public class MethodOption<M, T> {
      * @param keyFunction   键函数
      * @param valueFunction 值函数
      */
-    public MethodOption(final Class clazz, final Function<Method, M> keyFunction, final Function<Method, T> valueFunction) {
+    public MethodOption(final Class<?> clazz, final Function<Method, M> keyFunction, final Function<Method, T> valueFunction) {
         Objects.requireNonNull(clazz);
         Objects.requireNonNull(keyFunction);
         Objects.requireNonNull(valueFunction);
@@ -80,11 +81,11 @@ public class MethodOption<M, T> {
     /**
      * 构造选项
      *
-     * @param clazz
-     * @param keyFunction
-     * @param valueFunction
+     * @param clazz         接口类
+     * @param keyFunction   键函数
+     * @param valueFunction 值函数
      */
-    protected void buildOptions(final Class clazz, final Function<Method, M> keyFunction, final Function<Method, T> valueFunction) {
+    protected void buildOptions(final Class<?> clazz, final Function<Method, M> keyFunction, final Function<Method, T> valueFunction) {
         this.keyFunction = keyFunction;
         this.options = new HashMap<>();
         List<Method> methods = ClassUtils.getPublicMethod(clazz);
@@ -101,11 +102,22 @@ public class MethodOption<M, T> {
     /**
      * 获取缓存的数据
      *
-     * @param method
-     * @return
+     * @param method 方法对象
+     * @return 方法选项
      */
     public T get(final Method method) {
         return method == null || size == 0 ? null : options.get(keyFunction.apply(method));
+    }
+
+    /**
+     * 迭代选项
+     *
+     * @param consumer 消费者
+     */
+    public void forEach(final BiConsumer<M, T> consumer) {
+        if (consumer != null && options != null) {
+            options.forEach(consumer);
+        }
     }
 
 
@@ -119,10 +131,10 @@ public class MethodOption<M, T> {
         /**
          * 构造函数
          *
-         * @param clazz
-         * @param valueFunction
+         * @param clazz         接口类
+         * @param valueFunction 值函数
          */
-        public MethodKeyOption(Class clazz, Function<Method, T> valueFunction) {
+        public MethodKeyOption(Class<?> clazz, Function<Method, T> valueFunction) {
             super(clazz, METHOD_KEY, valueFunction);
         }
     }
@@ -155,25 +167,25 @@ public class MethodOption<M, T> {
         /**
          * 构造函数
          *
-         * @param clazz
-         * @param valueFunction
+         * @param clazz         接口类名
+         * @param valueFunction 值函数
          */
-        public NameKeyOption(final Class clazz, final Function<Method, T> valueFunction) {
+        public NameKeyOption(final Class<?> clazz, final Function<Method, T> valueFunction) {
             super(clazz, NAME_KEY, valueFunction);
         }
 
         /**
          * 构造函数
          *
-         * @param className
-         * @param nameFunction
+         * @param className    接口类名
+         * @param nameFunction 名称函数
          */
         public NameKeyOption(final String className, final Function<String, T> nameFunction) {
             this(null, className, nameFunction);
         }
 
         @Override
-        protected void buildOptions(final Class clazz, final Function<Method, String> keyFunction, final Function<Method, T> valueFunction) {
+        protected void buildOptions(final Class<?> clazz, final Function<Method, String> keyFunction, final Function<Method, T> valueFunction) {
             super.buildOptions(clazz, keyFunction, valueFunction);
             this.methodGetter = (method) -> size == 0 ? null : options.get(keyFunction.apply(method));
             this.nameGetter = (name) -> size == 0 ? null : options.get(name);
@@ -182,18 +194,18 @@ public class MethodOption<M, T> {
         /**
          * 构造函数
          *
-         * @param clazz
-         * @param className
-         * @param nameFunction
+         * @param clazz        接口类
+         * @param className    类名
+         * @param nameFunction 方法名称函数
          */
-        public NameKeyOption(final Class clazz, final String className, final Function<String, T> nameFunction) {
+        public NameKeyOption(final Class<?> clazz, final String className, final Function<String, T> nameFunction) {
             Objects.requireNonNull(nameFunction);
             if (className != null && clazz == null) {
                 this.className = className;
                 this.nameFunction = nameFunction;
                 this.options = new ConcurrentHashMap<>();
-                this.methodGetter = (method) -> options.computeIfAbsent(method.getName(), name -> nameFunction.apply(name));
-                this.nameGetter = (name) -> options.computeIfAbsent(name, n -> nameFunction.apply(n));
+                this.methodGetter = (method) -> options.computeIfAbsent(method.getName(), nameFunction);
+                this.nameGetter = (name) -> options.computeIfAbsent(name, nameFunction);
             } else if (clazz != null) {
                 buildOptions(clazz, NAME_KEY, method -> nameFunction.apply(keyFunction.apply(method)));
             } else {
@@ -210,8 +222,8 @@ public class MethodOption<M, T> {
         /**
          * 根据方法名称获取
          *
-         * @param name
-         * @return
+         * @param name 方法名称
+         * @return 选项
          */
         public T get(final String name) {
             return name == null ? null : nameGetter.apply(name);

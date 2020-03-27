b3c7c43381c3dbafb2dab0b7241e750059f17d03
bjhexiaofeng <hexiaofeng@jd.com>
2020-03-27 10:21:58 +0800

    注释

--- joyrpc-core/src/main/java/io/joyrpc/protocol/handler/BizReqHandler.java
+++ joyrpc-core/src/main/java/io/joyrpc/protocol/handler/BizReqHandler.java
@@ -101,6 +101,7 @@ public class BizReqHandler extends AbstractReqHandler implements MessageHandler 
             //从会话恢复
             restore(request, channel);
             //根据请求参数获取输出的服务，依赖于会话恢复的信息
+            //TODO exporter可以绑定到会话上加快性能
             exporter = InvokerManager.getExporter(invocation.getClassName(), invocation.getAlias(), channel.getLocalAddress().getPort());
             if (exporter == null) {
                 //如果本地没有该服务，抛出ShutdownExecption，让消费者主动关闭连接

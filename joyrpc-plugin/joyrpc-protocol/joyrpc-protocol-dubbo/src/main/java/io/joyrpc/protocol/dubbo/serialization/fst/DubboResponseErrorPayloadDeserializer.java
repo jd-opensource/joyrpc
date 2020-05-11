dc8cb8f5e73a27c733bc51c3841558cd4fc4fcdd
bjhexiaofeng <hexiaofeng@jd.com>
2020-05-11 10:27:38 +0800

    dubbo增加fst&kryo序列化

---
+++ joyrpc-plugin/joyrpc-protocol/joyrpc-protocol-dubbo/src/main/java/io/joyrpc/protocol/dubbo/serialization/fst/DubboResponseErrorPayloadDeserializer.java
@@ -0,0 +1,78 @@
+package io.joyrpc.protocol.dubbo.serialization.fst;
+
+/*-
+ * #%L
+ * joyrpc
+ * %%
+ * Copyright (C) 2019 joyrpc.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import io.joyrpc.codec.serialization.ObjectInputReader;
+import io.joyrpc.codec.serialization.ObjectOutputWriter;
+import io.joyrpc.protocol.dubbo.message.DubboResponseErrorPayload;
+import org.nustaq.serialization.AutowiredObjectSerializer;
+import org.nustaq.serialization.FSTClazzInfo;
+import org.nustaq.serialization.FSTObjectInput;
+import org.nustaq.serialization.FSTObjectOutput;
+
+import java.io.IOException;
+
+/**
+ * DubboResponseErrorPayload反序列化
+ */
+public class DubboResponseErrorPayloadDeserializer implements AutowiredObjectSerializer {
+
+    @Override
+    public Class<?> getType() {
+        return DubboResponseErrorPayload.class;
+    }
+
+    @Override
+    public void writeObject(final FSTObjectOutput out,
+                            final Object toWrite,
+                            final FSTClazzInfo clzInfo,
+                            final FSTClazzInfo.FSTFieldInfo referencedBy,
+                            final int streamPosition) throws IOException {
+        ((DubboResponseErrorPayload) toWrite).write(new ObjectOutputWriter(out));
+    }
+
+    @Override
+    public void readObject(final FSTObjectInput in,
+                           final Object toRead,
+                           final FSTClazzInfo clzInfo,
+                           final FSTClazzInfo.FSTFieldInfo referencedBy) throws Exception {
+        ((DubboResponseErrorPayload) toRead).read(new ObjectInputReader(in));
+    }
+
+    @Override
+    public boolean willHandleClass(final Class cl) {
+        return false;
+    }
+
+    @Override
+    public boolean alwaysCopy() {
+        return false;
+    }
+
+    @Override
+    public Object instantiate(final Class objectClass, final FSTObjectInput fstObjectInput,
+                              final FSTClazzInfo serializationInfo,
+                              final FSTClazzInfo.FSTFieldInfo referencee,
+                              final int streamPosition) throws Exception {
+        return new DubboResponseErrorPayload();
+    }
+
+}
---
+++ joyrpc-plugin/joyrpc-protocol/joyrpc-protocol-dubbo/src/main/java/io/joyrpc/protocol/dubbo/serialization/kryo/DubboResponseErrorPayloadDeserializer.java
@@ -0,0 +1,62 @@
+package io.joyrpc.protocol.dubbo.serialization.kryo;
+
+/*-
+ * #%L
+ * joyrpc
+ * %%
+ * Copyright (C) 2019 joyrpc.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.esotericsoftware.kryo.AutowiredObjectSerializer;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.KryoException;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import io.joyrpc.codec.serialization.kryo.KryoReader;
+import io.joyrpc.codec.serialization.kryo.KryoWriter;
+import io.joyrpc.protocol.dubbo.message.DubboResponseErrorPayload;
+
+import java.io.IOException;
+
+/**
+ * DubboResponseErrorPayload反序列化
+ */
+public class DubboResponseErrorPayloadDeserializer extends AutowiredObjectSerializer {
+
+    @Override
+    public Class<?> getType() {
+        return DubboResponseErrorPayload.class;
+    }
+
+    @Override
+    public void write(final Kryo kryo, final Output output, final Object object) {
+        try {
+            ((DubboResponseErrorPayload) object).write(new KryoWriter(kryo, output));
+        } catch (IOException e) {
+            throw new KryoException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Object read(final Kryo kryo, final Input input, final Class type) {
+        try {
+            return new DubboResponseErrorPayload().read(new KryoReader(kryo, input));
+        } catch (IOException e) {
+            throw new KryoException(e.getMessage(), e);
+        }
+    }
+
+}

89f5585cf67d885da0cfe4b6c4b83ef1976f88db
bjhexiaofeng <hexiaofeng@jd.com>
2020-05-11 08:04:33 +0800

    review

--- joyrpc-plugin/joyrpc-codec/joyrpc-serialization-hessian/src/main/java/io/joyrpc/codec/serialization/hessian2/Hessian2Writer.java
+++ joyrpc-plugin/joyrpc-codec/joyrpc-serialization-hessian/src/main/java/io/joyrpc/codec/serialization/hessian2/Hessian2Writer.java
@@ -9,9 +9,9 @@ package io.joyrpc.codec.serialization.hessian2;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +21,7 @@ package io.joyrpc.codec.serialization.hessian2;
  */
 
 import io.joyrpc.codec.serialization.ObjectWriter;
-import io.joyrpc.com.caucho.hessian.io.Hessian2Output;
+import io.joyrpc.com.caucho.hessian.io.AbstractHessianOutput;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -30,10 +30,12 @@ import java.nio.charset.Charset;
  * HessianLite 输出
  */
 public class Hessian2Writer implements ObjectWriter {
+    /**
+     * hessian2输出
+     */
+    protected final AbstractHessianOutput hessian2Output;
 
-    protected final Hessian2Output hessian2Output;
-
-    public Hessian2Writer(Hessian2Output hessian2Output) {
+    public Hessian2Writer(AbstractHessianOutput hessian2Output) {
         this.hessian2Output = hessian2Output;
     }
 

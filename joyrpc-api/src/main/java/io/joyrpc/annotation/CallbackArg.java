2cf8635e34f7c65a85d591662ba7f154a86a90e0
bjhexiaofeng <hexiaofeng@jd.com>
2020-05-15 14:34:48 +0800

    把异常，callback，genericservice移动到api模块，业务的代码里面会用到

--- joyrpc-api/src/main/java/io/joyrpc/annotation/CallbackArg.java
+++ joyrpc-api/src/main/java/io/joyrpc/annotation/CallbackArg.java
@@ -1,4 +1,4 @@
-package io.joyrpc;
+package io.joyrpc.annotation;
 
 /*-
  * #%L
@@ -9,9 +9,9 @@ package io.joyrpc;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,35 +20,18 @@ package io.joyrpc;
  * #L%
  */
 
+
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+
 /**
- * Title: 回调抽象类<br>
- * <p/>
- * Description: 实现通知方法，指定传递对象<br>
- * <p/>
+ * 回调参数注解
  */
-@FunctionalInterface
-public interface Callback<Q, S> {
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(PARAMETER)
+@Inherited
+public @interface CallbackArg {
 
-    /**
-     * 回调通知
-     *
-     * @param result 通知对象
-     * @return 返回值对象
-     */
-    S notify(Q result);
-
-    /**
-     * 连接断开的时候调用重新注册
-     */
-    default void recallback() {
-    }
-
-    /**
-     * 设置回调ID
-     *
-     * @param callbackId
-     */
-    default void setCallbackId(final String callbackId) {
-
-    }
 }

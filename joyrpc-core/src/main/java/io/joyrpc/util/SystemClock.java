5cde0fd96243cbdf440353f77acab91f1072fc2e
yangyang36 <yangyang36@jd.com>
2020-06-02 16:11:33 +0800

    review

--- joyrpc-core/src/main/java/io/joyrpc/util/SystemClock.java
+++ joyrpc-core/src/main/java/io/joyrpc/util/SystemClock.java
@@ -9,9 +9,9 @@ package io.joyrpc.util;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -77,4 +77,12 @@ public class SystemClock {
         return instance.getTime();
     }
 
+    /**
+     * 获取当前时钟，微妙
+     * @return
+     */
+    public static long nowMicros() {
+        long nanoTime = System.nanoTime(); // 纳秒
+        return now() + (nanoTime - nanoTime / 1000000 * 1000000) / 1000;
+    }
 }

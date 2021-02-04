4edfbab43a7ee2ed0321111f3bdb7aaafc3c65dc
hexiaofeng <hexiaofeng@139.com>
2021-02-04 11:30:50 +0800

    升级版本

--- joyrpc-test/joyrpc-test-transport/src/test/java/io/joyrpc/transport/netty4/test/heartbeat/TimingHeartbeatClientMain.java
+++ joyrpc-test/joyrpc-test-transport/src/test/java/io/joyrpc/transport/netty4/test/heartbeat/TimingHeartbeatClientMain.java
@@ -9,9 +9,9 @@ package io.joyrpc.transport.netty4.test.heartbeat;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -62,11 +62,7 @@ public class TimingHeartbeatClientMain {
                         return HeartbeatMode.TIMING;
                     }
                 });
-                try {
-                    client.open();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                client.open();
             }).start();
         }
 

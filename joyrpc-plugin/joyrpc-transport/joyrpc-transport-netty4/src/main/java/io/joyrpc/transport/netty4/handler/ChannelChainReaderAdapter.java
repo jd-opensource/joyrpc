d94f39a9a8695bbbec280530b2e58cafd95e9dc6
hexiaofeng <hexiaofeng@139.com>
2021-02-22 17:14:55 +0800

    review

--- joyrpc-plugin/joyrpc-transport/joyrpc-transport-netty4/src/main/java/io/joyrpc/transport/netty4/handler/ChannelChainReaderAdapter.java
+++ joyrpc-plugin/joyrpc-transport/joyrpc-transport-netty4/src/main/java/io/joyrpc/transport/netty4/handler/ChannelChainReaderAdapter.java
@@ -22,6 +22,7 @@ package io.joyrpc.transport.netty4.handler;
 
 import io.joyrpc.transport.channel.Channel;
 import io.joyrpc.transport.channel.ChannelChainReaderContext;
+import io.joyrpc.transport.channel.ChannelContext;
 import io.joyrpc.transport.channel.ChannelReader;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -51,7 +52,7 @@ public class ChannelChainReaderAdapter extends ChannelInboundHandlerAdapter {
      */
     protected final ThreadPoolExecutor executor;
 
-    public ChannelChainReaderAdapter(ChannelReader[] readers, Channel channel, ThreadPoolExecutor executor) {
+    public ChannelChainReaderAdapter(final ChannelReader[] readers, final Channel channel, final ThreadPoolExecutor executor) {
         this.readers = readers;
         this.channel = channel;
         this.executor = executor;
@@ -72,12 +73,50 @@ public class ChannelChainReaderAdapter extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(final ChannelHandlerContext ctx, final Object msg) throws Exception {
         ChannelChainReaderContext context = new ChannelChainReaderContext(channel, readers, create(channel, ctx));
-        context.fireChannelRead(msg);
+        if (executor != null) {
+            try {
+                executor.execute(new ReceiveJob(context, msg));
+            } catch (Throwable e) {
+                //可能抛出RejectedExecutionException
+                context.fireExceptionCaught(e);
+            }
+        } else {
+            context.fireChannelRead(msg);
+        }
     }
 
     @Override
     public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) throws Exception {
         ChannelChainReaderContext context = new ChannelChainReaderContext(channel, readers, create(channel, ctx));
         context.fireExceptionCaught(cause);
+    }
+
+    /**
+     * 收到数据的任务
+     */
+    protected static class ReceiveJob implements Runnable, Comparable {
+        /**
+         * 上下文
+         */
+        protected final ChannelContext context;
+        /**
+         * 消息
+         */
+        protected final Object message;
+
+        public ReceiveJob(final ChannelContext context, final Object message) {
+            this.context = context;
+            this.message = message;
+        }
+
+        @Override
+        public int compareTo(final Object o) {
+            return 0;
+        }
+
+        @Override
+        public void run() {
+            context.fireChannelRead(message);
+        }
     }
 }

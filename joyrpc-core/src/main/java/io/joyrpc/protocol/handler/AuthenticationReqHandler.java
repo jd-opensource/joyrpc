811d7df8ca677b1f0338a8bfb70971a4376e3560
hexiaofeng <hexiaofeng@139.com>
2021-02-24 18:55:25 +0800

    review

--- joyrpc-core/src/main/java/io/joyrpc/protocol/handler/AuthenticationReqHandler.java
+++ joyrpc-core/src/main/java/io/joyrpc/protocol/handler/AuthenticationReqHandler.java
@@ -29,7 +29,6 @@ import io.joyrpc.protocol.MessageHandler;
 import io.joyrpc.protocol.MsgType;
 import io.joyrpc.protocol.message.Message;
 import io.joyrpc.protocol.message.RequestMessage;
-import io.joyrpc.protocol.message.ResponseMessage;
 import io.joyrpc.protocol.message.authentication.AuthenticationRequest;
 import io.joyrpc.protocol.message.authentication.AuthenticationResponse;
 import io.joyrpc.transport.channel.Channel;
@@ -40,6 +39,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static io.joyrpc.protocol.MsgType.AuthenticationResp;
+import static io.joyrpc.protocol.message.ResponseMessage.build;
 import static io.joyrpc.protocol.message.authentication.AuthenticationResponse.NOT_PASS;
 import static io.joyrpc.protocol.message.authentication.AuthenticationResponse.PASS;
 
@@ -65,8 +65,7 @@ public class AuthenticationReqHandler extends AbstractReqHandler implements Mess
 
         RpcSession session = (RpcSession) request.getSession();
         if (session == null) {
-            channel.send(ResponseMessage.build(request, AuthenticationResp.getType(),
-                    new AuthenticationResponse(NOT_PASS, "Session is not exists.")), sendFailed);
+            acknowledge(channel, request, "Session is not exists.");
             return;
         }
         AuthenticationRequest authReq = (AuthenticationRequest) request.getPayLoad();
@@ -77,10 +76,7 @@ public class AuthenticationReqHandler extends AbstractReqHandler implements Mess
         Exporter exporter = ServiceManager.getExporter(className, alias, port);
         if (exporter == null) {
             //抛出异常
-            channel.send(ResponseMessage.build(request, AuthenticationResp.getType(),
-                    new AuthenticationResponse(NOT_PASS,
-                            String.format("exporter is not exists. class=%s,alias=%s,port=%d", className, alias, port))),
-                    sendFailed);
+            acknowledge(channel, request, String.format("exporter is not exists. class=%s,alias=%s,port=%d", className, alias, port));
             return;
         }
         //判断认证算法是否一致
@@ -89,25 +85,43 @@ public class AuthenticationReqHandler extends AbstractReqHandler implements Mess
         if (identification == null || authenticator == null) {
             //没有配置认证
             session.setAuthenticated(Session.AUTH_SESSION_SUCCESS);
-            channel.send(ResponseMessage.build(request, AuthenticationResp.getType(), new AuthenticationResponse(PASS)), sendFailed);
+            acknowledge(channel, request, new AuthenticationResponse(PASS));
         } else if (!identification.type().equals(authReq.getType())) {
             //身份类型不对
-            channel.send(ResponseMessage.build(request, AuthenticationResp.getType(),
-                    new AuthenticationResponse(NOT_PASS,
-                            String.format("identification must be %s. class=%s,alias=%s,port=%d",
-                                    identification.type(), className, alias, port))),
-                    sendFailed);
+            acknowledge(channel, request, String.format("identification must be %s. class=%s,alias=%s,port=%d",
+                    identification.type(), className, alias, port));
         } else {
             try {
                 AuthenticationResponse response = authenticator.authenticate(authReq);
                 session.setAuthenticated(response.isSuccess() ? Session.AUTH_SESSION_SUCCESS : Session.AUTH_SESSION_FAIL);
-                channel.send(ResponseMessage.build(request, AuthenticationResp.getType(), response), sendFailed);
+                acknowledge(channel, request, response);
             } catch (Exception e) {
-                channel.send(ResponseMessage.build(request, AuthenticationResp.getType(),
-                        new AuthenticationResponse(NOT_PASS, e.getMessage())), sendFailed);
+                acknowledge(channel, request, e.getMessage());
             }
         }
 
+    }
+
+    /**
+     * 应答消息
+     *
+     * @param channel  连接通道
+     * @param request  请求
+     * @param response 应答
+     */
+    protected void acknowledge(final Channel channel, final RequestMessage request, final AuthenticationResponse response) {
+        channel.send(build(request, AuthenticationResp.getType(), response), sendFailed);
+    }
+
+    /**
+     * 应答消息
+     *
+     * @param channel 连接通道
+     * @param request 请求
+     * @param error   错误
+     */
+    protected void acknowledge(final Channel channel, final RequestMessage request, final String error) {
+        channel.send(build(request, AuthenticationResp.getType(), new AuthenticationResponse(NOT_PASS, error)), sendFailed);
     }
 
     @Override
